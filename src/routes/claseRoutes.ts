cbe61b96b18e73f60f8e3e90f203ddbe00aa77fb
Sopapa <extrahuevordinariopro@gmail.com>
2025-05-13 08:28:43 -0300

    arreglamos login

--- src/routes/claseRoutes.ts
+++ src/routes/claseRoutes.ts
@@ -1,13 +1,15 @@
 // /routes/claseRoutes.ts
 import { Router } from 'express';
 import { getClases, getClaseById, createClase, updateClase, deleteClase } from '../controller/claseController';
+import { authenticate } from '../middlewear/authMiddleware'; // Importa el middleware de autenticación
 
 const router = Router();
 
-router.get('/clases', getClases);
-router.get('/clases/:id', getClaseById);
-router.post('/clases', createClase);
-router.put('/clases/:id', updateClase);
-router.delete('/clases/:id', deleteClase);
+// Aplica el middleware 'authenticate' a todas las rutas de clases
+router.get('/clases', authenticate, getClases);
+router.get('/clases/:id', authenticate, getClaseById);
+router.post('/clases', authenticate, createClase);
+router.put('/clases/:id', authenticate, updateClase);
+router.delete('/clases/:id', authenticate, deleteClase);
 
 export default router;
